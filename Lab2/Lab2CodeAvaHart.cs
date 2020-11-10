945ca03ae5104b7ded1d1e42ce8990672312b3e9
Ava Hartney <S01193997@acad.tri-c.edu>
2020-11-09 20:34:54 -0500

    Lab 02 Completed

---
+++ Lab2/Lab2CodeAvaHart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        int number1; //declare first number to multiply
+        int number2; //declare second number to multiply
+
+        Console.Write { "Enter first integer:"}; // prompt user //read firstnumber from user
+        number1 = Convert.ToInt32 { Console.ReadLine() };
+
+        Console.Write { "Enter second interger:"};
+        number2 = Convet.toint32 { Console.ReadLine() };
+
+        sum = number1 * number2; //multiply numbers
+
+        Console.WriteLine { "Sum is {0}", sum}; //display sim
+
+    }
+    {
+        Console.WriteLine({ 0}/ n{ 1}, "Hello World!", "From Ava")
+	}
+
+// a. Our application ends at the last bracket on line 23.
+// b. Integer variables must show an integer value, or a whole number. Floating point variables are a real number with a decimal point which can come before or after the numbers.
+// c. An example of a method can be shown in lines 10, 13, 18, and 22 when the Console.WriteLine action was used.
+// d. Get accessor and set accessor.
+// e. A class is a collection of similar objects and has no memory allocated for it. An object has its own memory and cannot exist without a class. Classes do not have values associated with the fields, but every object does. There is no limit on how many objects in a clas can be created, though computer memory will limit it.
---
+++ Lab3Code/Lab3Code/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Both single-selection and while statements have to figure out is something is true or false. Contrarily, a single-selection only does what is being asked of the program, while a while loop keeps executing over and over until the loop is terminated.
+
+            int speedLimit;
+            int speed;
+
+            speedLimit = 35;
+            speed = 42;
+
+            if (speed > speedLimit)
+                Console.Write("SLOW NOW");
+
+            int istrue = 10;
+
+            if (istrue <= 60)
+                Console.WriteLine("It is True");
+            else
+                Console.WriteLine("It is False");
+
+
+            {
+                Console.WriteLine("Input Temperature in Fahrenheit:");
+
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+
+                double Celsius = (fahrenheit - 32d) * 5d / 9d;
+
+                Console.WriteLine("Temperature in Celsius is {0}:", Celsius);
+
+                Console.ReadLine();
+
+                if (fahrenheit >= 90)
+
+                    Console.WriteLine("It is hot");
+
+                if (fahrenheit <= 40)
+                {
+
+                    int i = 1;
+
+                    while (i < 11)
+                    {
+                        Console.Write("While Output:");
+                        Console.WriteLine(i);
+                        i++;
+                    }
+
+                    int x = 60;
+
+                    while (x >= 20)
+                    {
+                        Console.Write("While Output:");
+                        Console.WriteLine(i);
+                        i--;
+                    }
+
+                    int y = 10;
+
+                    while (y <= 20)
+                    {
+                        Console.Write("While Output:");
+                        Console.WriteLine(i);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
