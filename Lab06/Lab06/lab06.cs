e6e645aa2567ba28a577d4536385806af09c43cb
Ava Hartney <S01193997@acad.tri-c.edu>
2020-11-30 22:47:05 -0500

    completed lab06

---
+++ Lab06/Lab06/lab06.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lab06
+{
+    class Program
+    {
+        private static string[] months, seasons, array, names;
+
+        private static object Main(string[months] args)
+        {
+            months = new String[12];
+            months[0] = "january";
+            months[1] = "february";
+            months[2] = "march";
+            months[3] = "april";
+            months[4] = "may";
+            months[5] = "june";
+            months[6] = "july";
+            months[7] = "august";
+            months[8] = "september";
+            months[9] = "october";
+            months[10] = "november";
+            months[11] = "december";
+
+            seasons = new string[4];
+            seasons[0] = "spring";
+            seasons[1] = "fall";
+            seasons[2] = "summer";
+            seasons[3] = "winter";
+
+            for (int i = 0; i < seasons; i++)
+            {
+                Console.WriteLine("= foreach loop")
+            }
+
+            array = new string[1000];
+            array[Random]
+
+
+            Random random = new Random();
+            int randomnumber
+               for (string randomnumber = randm.Next(0, 1000);
+
+
+
+           new string[10] names; = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            int i = 0;
+            while (i < names.Length)
+            {
+                Console.WriteLine(names"{0, 2}, {1"), i, names[i]);
+            }
+
+
+
+        }
+
+
+    }
+}
+}
+//A one-dimesional array p contains four elements. The array access expressions to access eacho fo the elements in p are p[0}, p{1}, p{2}, and p{3}
+
